6ead67372220a5b112c3bbf1912d402a88b73389
Robinkhilery960 <robinkhilery960@gmail.com>
2023-01-22 21:15:17 +0530

    connected to database

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+  
 
 function App() {
   return (
     <div className="App">
-       
+      <div className="main">
+        <h1>Hello client </h1>
+      </div>
     </div>
   );
 }
---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+const {MONGODB_URL}=process.env
+exports.connect=()=>{
+    mongoose.connect(MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }).then(()=>{
+        console.log("connection successful")
+    }).catch((error)=>{
+        console.log("connection failed")
+        console.log(error)
+    })
+}
