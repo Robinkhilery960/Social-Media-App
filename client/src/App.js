035afeb20a0f02fdc7c8e75d71299efd293d9c95
Robinkhilery960 <robinkhilery960@gmail.com>
2023-01-22 23:02:16 +0530

    setup custom router for rendering page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-  
+ import {BrowserRouter as Router, Route,Routes} from "react-router-dom"; 
+ import PageRender from "./pageRender.js";
+ import Login from "./pages/login.js";
+import Register from "./pages/register.js";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="main">
-        <h1>Hello client </h1>
-      </div>
-    </div>
+  return ( 
+    <Router>
+      <input type="checkbox" id="theme" />
+      <div className="App">
+        <div className="main">
+          <h1>Hello client </h1>
+          <Routes>  
+          <Route path="/:page" element={<PageRender/>}/>  
+          <Route path="/:page/:id" element={<PageRender/>}/>  
+          </Routes>
+          </div>
+      </div> 
+      </Router>
   );
 }
 
