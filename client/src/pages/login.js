ce5d4bfcdd5fe3c2b26009c4b476ece4b73d23ac
Robinkhilery960 <robinkhilery960@gmail.com>
2023-01-30 21:33:20 +0530

    pages-login ui  and  api calls and managing data using redux

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { login } from "../redux/actions/authAction";
+import { useDispatch } from "react-redux";
 
 function Login() {
+  const initialState = { email: "", password: "" };
+
+  const [userData, setUserData] = useState(initialState);
+
+  const [typePass, setTypePass] = useState(false);
+
+  const { email, password } = userData;
+
+  const dispatch = useDispatch();
+
+  const handleChangeInput = (e) => {
+    console.log(e.target);
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(userData));
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="auth_page">
+      <h3 className="text-uppercase text-center mb-2">The NETWORK</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail">Email Address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={handleChangeInput}
+            name="email"
+            value={email}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else{" "}
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword">Password</label>
+          <div className="pass">
+            <input
+              type={typePass ? "text" : "password"}
+              className="form-control"
+              id="exampleInputPassword1"
+              aria-describedby="passwordHelp"
+              onChange={handleChangeInput}
+              name="password"
+              value={password}
+            />
+            <small onClick={() => setTypePass(!typePass)}>
+              {typePass ? "Hide" : "show"}
+            </small>
+          </div>
+          
+        </div>
+        <button
+          type="submit"
+          className="btn btn-dark  w-100"
+          disabled={email && password ? false : true}
+        >
+          Login
+        </button>
+        <p className="my-2">
+          You don't have an account?{" "}
+          <Link to="/register" style={{ color: "crimson" }}>
+            Register Now
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
---
+++ client/src/redux/actions/authAction.js
@@ -0,0 +1,79 @@
+import { postDataApi } from "../../utils/fetchData";
+import valid from "../../utils/valid";
+import { GLOBALTYPES } from "./globalTypes";
+
+
+export const login = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+    const res = await postDataApi("login", data);
+    //  console.log(res)
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: { token: res.data.access_token, user: res.data.user },
+    });
+
+    localStorage.setItem("firstLogin", true);
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { success: res.data.msg } });
+  } catch (error) {
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: { error: error.response.data.msg },
+    });
+  }
+};
+
+// export function login(data){
+//     return function(dispatch){
+//         console.log(dispatch())
+//     }
+// }
+
+export const refreshToken = () => async (dispatch) => {
+  const firstLogin = localStorage.getItem("firstLogin");
+  if (firstLogin) {
+    try {
+      dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+      const res = await postDataApi("refresh_token");
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: { token: res.data.access_token, user: res.data.user },
+      });
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {},
+      });
+    } catch (error) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: { error: error.response.data.msg },
+      });
+    }
+  }
+};
+
+export const register = (data) => async (dispatch) => {
+    const check=valid(data)
+    // console.log(check)
+    if(check.errLength>0){
+        return dispatch({type:GLOBALTYPES.ALERT,payload:check.errMsg})
+    }
+  try {
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+    const res = await postDataApi("register", data);
+    //  console.log(res)
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: { token: res.data.access_token, user: res.data.user },
+    });
+
+    localStorage.setItem("firstLogin", true);
+
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { success: res.data.msg } });
+  } catch (error) { 
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: { error: error.response.data.msg },
+    });
+  }
+};
