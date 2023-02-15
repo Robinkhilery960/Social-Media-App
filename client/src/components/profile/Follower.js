0eae5e85baf53126cb122ed9bda0b2848a587c89
Robinkhilery960 <robinkhilery960@gmail.com>
2023-02-15 10:37:35 +0530

    showFollowers, showFollowings

---
+++ client/src/components/profile/Follower.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import FollowBtn from '../FollowBtn'
+import UserCard from "../UserCard"
+const Follower = ({users,setShowFollower}) => {
+    const {auth} =useSelector(state=>state)
+  return (
+    <div className="follow ">
+        <div className="follow_box">
+            <h5>Followers</h5>
+            <hr />
+            {
+                users.map((user)=>(
+                    <UserCard key={user._id} user={user} setShowFollower={setShowFollower}>
+                        {
+                            
+                            auth.user._id!==user._id && <FollowBtn user={user}/>
+                        }
+                    </UserCard>
+                ))
+            }
+        <div className="close" onClick={()=>setShowFollower(false)}>
+            &times;
+        </div>
+        </div> 
+    </div>
+  )
+}
+
+export default Follower
