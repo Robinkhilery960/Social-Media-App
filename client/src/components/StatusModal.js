6027b57da225ac7474d2c0da4f48482a983e8368
Robinkhilery960 <robinkhilery960@gmail.com>
2023-02-27 16:43:08 +0530

    Status and StatusModal setup

---
+++ client/src/components/StatusModal.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { GLOBALTYPES } from "../redux/actions/globalTypes";
+import { useSelector, useDispatch } from "react-redux";
+import "../styles/statusModal.css";
+
+const StatusModal = () => {
+  const [content, setContent] = useState("");
+  const [images,setImages]=useState([])
+  const [stream, setStream]=useState(false)
+  const { auth, theme } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+
+  const handleChangeImages=(e)=>{
+    console.log(e.target.files)
+    const files=[...e.target.files]
+    let err=""
+    let newImages=[]
+
+    files.forEach((file)=>{
+        // if file is not found
+        if(!file) return err="File does not exist"
+        // if file type is incorrect 
+        if(file.type!=="image/jpeg" && file.type!=="image/jpg" && file.type!=="image/png"){
+          return err="File formate is incorrect"
+        }
+        return newImages.push(file) 
+    })
+
+    // if err found
+    if(err)   dispatch({type:GLOBALTYPES.ALERT,payload:{error:err}})
+
+    setImages([...images,...newImages])
+    // console.log(images)
+    // console.log(newImages)
+  }
+
+  const deleteImages=(index)=>{
+    // console.log(index)
+    const newArr=[...images]
+    newArr.splice(index, 1)
+    setImages(newArr)
+  }
+
+
+  return (
+    <div className="status_modal">
+      <form>
+        <div className="status_header">
+          <h5 className="m-0">Create Post</h5>
+          <span
+            onClick={() =>
+              dispatch({ type: GLOBALTYPES.STATUS, payload: false })
+            }
+          >
+            &times;
+          </span>
+        </div>
+        <div className="status_body">
+          <textarea
+            name="content"
+            placeholder={`${auth.user.fullName},What are you thinking ?`}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <div className="show_images">
+            {
+              images.map((image, index)=>(
+                <div key={index} id="file_img">
+                  <img src={URL.createObjectURL(image)} alt="images" className="img_thumbnail" style={{filter:theme?"invert(1)":"invert(0)"}}/>
+                  <span onClick={()=>deleteImages(index)}>&times;</span>
+                </div>
+              ))
+            }
+          </div>
+          <div className="input_images">
+            {/* TODO:Capture image from   video  */}
+            <i className="fas fa-camera" onClick={()=>setStream(true)} />
+            <div className="file_upload">
+              <i className="fas fa-image"></i>
+              <input
+                type="file"
+                name="file"
+                id="file"
+                multiple
+                accept="images/*"
+                onChange={(e)=>handleChangeImages(e)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="status_footer my-1">
+          <button className="btn btn-secondary w-100">Post</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default StatusModal;
---
+++ client/src/components/home/Status.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import Avatar from '../Avatar' 
+import { GLOBALTYPES } from '../../redux/actions/globalTypes'
+
+const Status = () => {
+    const {auth}=useSelector(state=>state)
+    const dispatch=useDispatch()
+  return (
+    <div className="status my-3 d-flex">
+        <Avatar src={auth.user.avatar} size="bg-avatar"/> 
+        <button className="statusBtn flex-fill" onClick={()=>dispatch({type:GLOBALTYPES.STATUS,payload:true})}>
+            {auth.user.fullName}, what are you thinking?
+        </button>
+    </div>
+  )
+}
+
+export default Status
