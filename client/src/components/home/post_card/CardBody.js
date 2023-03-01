04a6f9530e8f04b935925ac7876011a369a2ce89
Robin Khilery <96528134+Robinkhilery960@users.noreply.github.com>
2023-03-01 10:01:08 +0000

    cardBody

--- client/src/components/home/post_card/CardBody.js
+++ client/src/components/home/post_card/CardBody.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Carousel from '../../Carousel'
 const CardBody = ({post}) => {
+  
+  const [readMore, setReadMore] = useState(false);
   return (
     <div className='card_body'>
       <div className="card_body_content">
-        <span>{post.content}
+        <span>
+          {
+            post.content.length<60 ?post.content: readMore?post.content+"":post.content.slice(0, 60)+"....."
+          } 
         </span>
+        {
+          post.content.length>60 && 
+          <span className='readMore' onClick={setReadMore(!readMore)}>
+                {readMore?"Hide Content":"Read More"}
+          </span>
+        }
       </div> 
       {
         post.images.length>0 &&
